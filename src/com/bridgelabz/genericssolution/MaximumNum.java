6e13e9a9896b50cdc0b16ff9238b69d6d64c3456
rajshetye11 <rajshetye11@gmail.com>
2021-08-21 00:30:59 +0530

    [raj] : added Maximum number using Strings

--- src/com/bridgelabz/genericssolution/MaximumNum.java
+++ src/com/bridgelabz/genericssolution/MaximumNum.java
@@ -2,7 +2,7 @@ package com.bridgelabz.genericssolution;
 
 public class MaximumNum {
 	
-	public void maxNumbers(Float n1, Float n2,Float n3) {
+	public void maxNumbers(String n1, String n2,String n3) {
 		if((n1.compareTo(n2)>=0) && (n1.compareTo(n3)>=0))
 		{
 			System.out.println(n1+" is maximum number");
@@ -18,9 +18,9 @@ public class MaximumNum {
 	}
 	public static void main(String[] args) {
 	
-		Float n1 = new Float(22.0);
-		Float n2 = new Float(69.11); 
-		Float n3 = new Float(11.15);
+		String n1 = new String("Apple");
+		String n2 = new String("Peach"); 
+		String n3 = new String("Banana");
 		
 		MaximumNum mn = new MaximumNum();
 		mn.maxNumbers(n1, n2, n3);
