f711d126071e468bebe4f92af6c35dfa7e16ef63
rajshetye11 <rajshetye11@gmail.com>
2021-08-21 00:38:22 +0530

    [raj] : Refactored maximum numbers using Generics

--- src/com/bridgelabz/genericssolution/MaximumNum.java
+++ src/com/bridgelabz/genericssolution/MaximumNum.java
@@ -2,28 +2,33 @@ package com.bridgelabz.genericssolution;
 
 public class MaximumNum {
 	
-	public void maxNumbers(String n1, String n2,String n3) {
-		if((n1.compareTo(n2)>=0) && (n1.compareTo(n3)>=0))
-		{
-			System.out.println(n1+" is maximum number");
-		}
-		else if((n2.compareTo(n1)>=0) && (n2.compareTo(n3)>=0))
-		{
-			System.out.println(n2+" is maximum number");
-		}
-		else if((n3.compareTo(n1)>=0) && (n3.compareTo(n2)>=0))
-		{	
-			System.out.println(n3+" is maximum number"); 
-		}
-	}
+	public <T extends Comparable<T>> void maxNumbers (T n1, T n2, T n3){
+        if ((n1.compareTo(n2) >= 0) && (n1.compareTo(n3) >= 0)){
+                System.out.println( n1 + " is maximum number");
+        }
+        else if ((n2.compareTo(n1) >= 0) && (n2.compareTo(n1) >= 0)) {
+            System.out.println( n2 + " is maximum number");
+        }
+        else if ((n3.compareTo(n1) >= 0) && (n3.compareTo(n2) >= 0))
+            System.out.println( n3 + " is maximum number");
+    }
 	public static void main(String[] args) {
-	
-		String n1 = new String("Apple");
-		String n2 = new String("Peach"); 
-		String n3 = new String("Banana");
+		Integer n1 = new Integer(22);
+		Integer n2 = new Integer(69); 
+		Integer n3 = new Integer(11);
+		
+		Float n4 = new Float(22.0);
+		Float n5 = new Float(69.11); 
+		Float n6 = new Float(11.15);
+		
+		String n7 = new String("Apple");
+		String n8 = new String("Peach"); 
+		String n9 = new String("Banana");
 		
 		MaximumNum mn = new MaximumNum();
 		mn.maxNumbers(n1, n2, n3);
+		mn.maxNumbers(n4, n5, n6);
+		mn.maxNumbers(n7, n8, n9);
 		
 	}
 }
