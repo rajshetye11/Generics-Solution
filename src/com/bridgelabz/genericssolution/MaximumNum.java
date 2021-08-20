20fa3dce7460f33251012620dccfe033b735a1e3
rajshetye11 <rajshetye11@gmail.com>
2021-08-21 00:26:43 +0530

    [raj] : added Maximum number using float

--- src/com/bridgelabz/genericssolution/MaximumNum.java
+++ src/com/bridgelabz/genericssolution/MaximumNum.java
@@ -2,7 +2,7 @@ package com.bridgelabz.genericssolution;
 
 public class MaximumNum {
 	
-	public void maxNumbers(Integer n1, Integer n2, Integer n3) {
+	public void maxNumbers(Float n1, Float n2,Float n3) {
 		if((n1.compareTo(n2)>=0) && (n1.compareTo(n3)>=0))
 		{
 			System.out.println(n1+" is maximum number");
@@ -13,14 +13,14 @@ public class MaximumNum {
 		}
 		else if((n3.compareTo(n1)>=0) && (n3.compareTo(n2)>=0))
 		{	
-			System.out.println(n3+" is maximum number");
+			System.out.println(n3+" is maximum number"); 
 		}
 	}
 	public static void main(String[] args) {
 	
-		Integer n1 = new Integer(22);
-		Integer n2 = new Integer(69); 
-		Integer n3 = new Integer(11);
+		Float n1 = new Float(22.0);
+		Float n2 = new Float(69.11); 
+		Float n3 = new Float(11.15);
 		
 		MaximumNum mn = new MaximumNum();
 		mn.maxNumbers(n1, n2, n3);
